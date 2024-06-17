d7e360891fe7b8e7b3a2e24337603a81b2538d2d
Mathieu <mathieuvantuil2000@gmail.com>
2024-06-17 14:58:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Angularproject';
+export class AppComponent implements OnInit {
+  data: any;
+
+  constructor(private dataService: DataService) {}
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((result) => {
+      this.data=result;
+    
+    });
+  }
+  title = 'The blog';
 }
